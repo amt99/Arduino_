ab57b6379cc3bf9a1b632bf382b04dbedc3301be
amt99 <almistro.9940@gmail.com>
2023-03-08 21:51:02 -0500

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,12 +1,15 @@
+int LED = 2
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT); 
+  pinMode(LED, OUTPUT); 
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on by setting the voltage level to HIGH
+  digitalWrite(LED, HIGH);  // turn the LED on by setting the voltage level to HIGH
   delay(1000);                      // wait for 1 second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by setting the voltage level to LOW
+  digitalWrite(LED, LOW);   // turn the LED off by setting the voltage level to LOW
   delay(1000);                      // wait for 1 second
 }
